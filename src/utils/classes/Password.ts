81304997b1384d773a4ebdc8ba82409e32565085
Adrien M <adrien.mld@laposte.net>
2021-12-19 20:18:17 +0100

    📦 NEW: class pour les mdp

---
+++ src/utils/classes/Password.ts
@@ -0,0 +1,11 @@
+import * as argon2 from "argon2";
+
+export default class Password {
+  public static async hash(password: string) {
+    return await argon2.hash(password, { saltLength: 10 });
+  }
+
+  public static async compare(password: string, hash: string) {
+    await argon2.verify(hash, password);
+  }
+}
