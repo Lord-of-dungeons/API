f9aafbaab55d696a38927b1483949e2e6387b9fa
Adrien M <adrien.mld@laposte.net>
2021-12-20 20:44:31 +0100

    📦 NEW: migration user newletter

---
+++ src/migrations/1640029432059-user_v4.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class userV41640029432059 implements MigrationInterface {
+    name = 'userV41640029432059'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `user` CHANGE `newsletter` `newsletter` tinyint NOT NULL DEFAULT '1'", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `user` CHANGE `newsletter` `newsletter` tinyint NULL DEFAULT '1'", undefined);
+    }
+
+}
